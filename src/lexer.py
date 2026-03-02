8bfa4cbdf31f1dd097edd5c1ca3e02449f12323b
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-03-01 20:31:43 -0600

    Fix and refactor: improve readability using the new splitter. (Fixes #9)

--- src/lexer.py
+++ src/lexer.py
@@ -55,120 +55,88 @@ def Lexer(src: str):
     tokens = []
     splitter = Splitter(src)
 
-    while not splitter.isOutOfBounds(splitter.current_position + 1):
-        #deberia quiza agregar verificacion aqui o en el splitter para que el src no sea algo vacio???
-
-        token = []
+    while splitter.has_more():
         current_char = splitter.current_char
+        start_row = splitter.current_row
+        start_col = splitter.current_column
+        
+        # usar esta lista para construir los tokens
+        generatedToken = []
 
         match current_char:
 
-            #si es una letra
-            case char if char.isalpha():
-                #averiguar si el siguiente caracter es caracter y si si, seguir agregando como token
-                while True:
-                    if( (splitter.peek_next().isalpha()) == False ):
-                        # añadir el caracter actual antes de salir
-                        token.append(splitter.current_char)
-
-                        #no tokentype pasado porque se espera que pueda ser algun keyword, sino sera ID
-                        tokens.append(buildToken(
-                            col=splitter.current_column,
-                            row=splitter.current_row,
-                            value=token 
-                        ))
-
-                        break #terminar el while si el siguiente caracter no es letra
-                    
-                    token.append(splitter.current_char)
-                    splitter.next_char();
-            
-            #si es un numero
-            case char if char.isdigit():
-                #averiguar si el siguiente caracter es digito y si si, seguir agregando como token
-                while True:
-                    if not splitter.peek_next().isdigit():
-                        # añadir el caracter actual antes de salir
-                        token.append(current_char)
-                        tokens.append(buildToken(
-                            row=splitter.current_row,
-                            col=splitter.current_column,  
-                            value=token,
-                            type=TokenType.NUMBER
-                        ))
-                        break #terminar el while si el siguiente caracter no es letra
-                    
-                    token.append(splitter.current_char)
-                    splitter.next_char();
-                    current_char = splitter.current_char
-
+            # whitespace
             case char if char.isspace():
-                #si es un espacio
+                # dejar al splitter.next_char() actuar al final
                 pass
 
-            case '"': # Comilla doble
-                splitter.next_char(); #salta el caracter DQ
-                while True:
-                    
-                    #Aqui pudiera haber otro raise Error si no queremos que haya algun otro " en medio
-                    #si el caracter actual no es otro DQ
-                    #if( (splitter.current_char == "\"") == False ):
-                        #pass
-
-                    #Se encuentra el DQ de cierre 
-                    if( splitter.current_char == "\"" ):
-                        #saltamos el DQ de cierre y no lo almacenamos
-                        #token.append(splitter.current_char)
-
-                        #se añade el token a los tokens como un STRING
-                        tokens.append(buildToken(
-                            row=splitter.current_row,
-                            col=splitter.current_column,  
-                            value=token,
-                            type=TokenType.STRING_LITERAL
-                        ))
-
-                        break #termina el while
-                        #TODO dado caso que no encuentre el token de cierre que hariamos?
-                    
-
-                    token.append(splitter.current_char)
-                    splitter.next_char();
-
-            case _: # Operadores o cualquier otro caracter
-                #si es una operador
-                possible_operator = TokenType.keyword_exists(current_char)
-
-                if( possible_operator ):
-                    #resulto que si era operador
-                    token.append(splitter.current_char)
-                    tokens.append(buildToken(
-                        row=splitter.current_row,
-                        col=splitter.current_column,  
-                        value=token,
-                        type=possible_operator
-                    ))
-                else: # Caracteres no reconocidos
-                    #TODO Asegurarse que el parsing se detenga en estos casos
-                    # quiza usar directamente un raise sea lo correcto?
-                    print(
-                        "[LEXER] Caracter no reconocido: " \
-                        f"value: {current_char}, " \
-                        f"row: {splitter.current_row} col: {splitter.current_column}\n"
-                    )
+            # palabras/keywords
+            case char if char.isalpha():
+                generatedToken.append(current_char)
                 
+                while splitter.has_more() and splitter.peek_next().isalpha():
+                    splitter.next_char()
+                    generatedToken.append(splitter.current_char)
+                
+                tokens.append(buildToken(start_row, start_col, generatedToken))
 
+            # numbers
+            case char if char.isdigit():
+                generatedToken.append(current_char)
+
+                while splitter.has_more() and splitter.peek_next().isdigit():
+                    splitter.next_char()
+                    generatedToken.append(splitter.current_char)
+                
+                tokens.append(buildToken(start_row, start_col, generatedToken, TokenType.NUMBER))
+
+            # strings "..."
+            case '"':
+                # saltar en DQ de apertura
+                splitter.next_char() 
+                
+                # Consumir hasta encontrar otro DQ o EOF
+                while splitter.has_more() and splitter.current_char != '"':
+                    generatedToken.append(splitter.current_char)
+                    
+                    # Si el sig caracter es DQ detenerse (sale del while)
+                    if splitter.peek_next() == '"':
+                        break
+                    splitter.next_char()
+                
+                # Moverse una vez para llegar al DQ de cierre
+                splitter.next_char() 
+                
+                if splitter.current_char == '"':
+                    tokens.append(buildToken(
+                        start_row, start_col,
+                        generatedToken, 
+                        TokenType.STRING_LITERAL
+                    ))
+                else:
+                    print(f"Error: Unclosed string at {start_row}:{start_col}")
+
+            # Simbolos/Operadores o caracteres no reconocidos
+            case _:
+                symbol = TokenType.keyword_exists(current_char)
+                if symbol:
+                    tokens.append(buildToken(
+                        start_row,
+                        start_col, 
+                        [current_char], 
+                        symbol
+                    ))
+                else:
+                    print(f"Unknown character: {current_char} at {start_row}:{start_col}")
+
+        # Esto mueve el puntero una vez por iteracion
         splitter.next_char()
 
-
-
-
-    #agregar token EOF (end of file)
+    # Final EOF Token
     tokens.append(buildToken(
-        row=splitter.current_row + 1,
-        col=0,  
-        value=["EOF"],
-        type=TokenType.EOF
+        splitter.current_row, 
+        1, 
+        ["EOF"], 
+        TokenType.EOF
     ))
-
     return tokens
